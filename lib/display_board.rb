27b53aa3e65ddd3108bb2e47684e158b8b211ea2
Luis Zabala <luis.zabala076@gmail.com>
2019-12-02 21:55:09 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -8,4 +8,4 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+board = ["X", "X", "X", "O", " ", "O", " ", "O", " "]
